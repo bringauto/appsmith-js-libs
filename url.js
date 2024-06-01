a47a948312078ec0569bca30bb94b8c3d2fe1890
Jan Kubalek <janmkubalek@gmail.com>
2024-06-01 12:33:33 +0200

    Update

--- url.js
+++ url.js
@@ -6,3 +6,13 @@ export function URLCreate(url) {
 export function ConcentateWithNewline(arrayList) {
     return arrayList.join('\n');
 }
+
+export async function loadLibrary(jsLibUrl) {
+    try {
+      const response = await fetch(jsLibUrl);
+      const scriptText = await response.text();
+      return eval(scriptText);
+    } catch (error) {
+      console.error(`Error loading library: ${error}`);
+    }
+}
